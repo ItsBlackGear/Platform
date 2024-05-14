c6b04ee5e08d965de388a2b70bfaa6aed31f7816
ItsBlackGear <itsblackgear@gmail.com>
2024-05-14 03:20:18 -0400

    🐛 Code cleanup

--- common/src/main/java/com/blackgear/platform/Platform.java
+++ common/src/main/java/com/blackgear/platform/Platform.java
@@ -1,6 +1,5 @@
 package com.blackgear.platform;
 
-import com.blackgear.platform.common.CommonSetup;
 import com.blackgear.platform.common.worldgen.modifier.BiomeManager;
 import com.blackgear.platform.core.ModInstance;
 import com.blackgear.platform.core.util.config.ConfigLoader;
@@ -10,9 +9,7 @@ import org.apache.logging.log4j.Logger;
 public class Platform {
 	public static final String MOD_ID = "platform";
 	public static final Logger LOGGER = LogManager.getLogger(MOD_ID);
-	public static final ModInstance INSTANCE = ModInstance.create(MOD_ID)
-		.postCommon(CommonSetup::postStartup)
-		.build();
+	public static final ModInstance INSTANCE = ModInstance.create(MOD_ID).build();
 
 	public static void bootstrap() {
 		INSTANCE.bootstrap();
