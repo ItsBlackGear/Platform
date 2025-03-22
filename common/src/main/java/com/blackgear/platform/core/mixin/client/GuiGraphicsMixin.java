9304a8d24177520e0114c54ee7f73a1b70b2f9e2
ItsBlackGear <68313143+ItsBlackGear@users.noreply.github.com>
2025-03-22 18:51:58 -0300

    🚀 v1.2.5
    🚀 v1.2.5
    - Version bump to 1.2.5
    - Parity with 1.16.5 and 1.19.2

--- common/src/main/java/com/blackgear/platform/core/mixin/client/GuiGraphicsMixin.java
+++ common/src/main/java/com/blackgear/platform/core/mixin/client/GuiGraphicsMixin.java
@@ -1,8 +1,6 @@
 package com.blackgear.platform.core.mixin.client;
 
 import com.blackgear.platform.client.GameRendering;
-import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
-import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.resources.model.BakedModel;
 import net.minecraft.world.entity.LivingEntity;
@@ -11,17 +9,18 @@ import net.minecraft.world.level.Level;
 import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(GuiGraphics.class)
 public abstract class GuiGraphicsMixin {
-    @WrapOperation(
+    @Redirect(
         method = "renderItem(Lnet/minecraft/world/entity/LivingEntity;Lnet/minecraft/world/level/Level;Lnet/minecraft/world/item/ItemStack;IIII)V",
         at = @At(
             value = "INVOKE",
             target = "Lnet/minecraft/client/resources/model/BakedModel;usesBlockLight()Z"
         )
     )
-    private boolean onRenderItem(BakedModel instance, Operation<Boolean> original, @Nullable LivingEntity entity, @Nullable Level level, ItemStack stack) {
-        return !GameRendering.SPECIAL_ITEMS.containsKey(stack.getItem()) && original.call(instance);
+    private boolean onRenderItem(BakedModel instance, @Nullable LivingEntity entity, @Nullable Level level, ItemStack stack) {
+        return !GameRendering.HAND_HELD_MODELS.containsKey(stack.getItem()) && instance.usesBlockLight();
     }
 }
--- common/src/main/java/com/blackgear/platform/core/mixin/client/ItemRendererMixin.java
+++ common/src/main/java/com/blackgear/platform/core/mixin/client/ItemRendererMixin.java
@@ -1,11 +1,6 @@
 package com.blackgear.platform.core.mixin.client;
 
 import com.blackgear.platform.client.GameRendering;
-import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
-import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
-import com.llamalad7.mixinextras.sugar.Local;
-import com.llamalad7.mixinextras.sugar.ref.LocalRef;
-import com.mojang.blaze3d.vertex.PoseStack;
 import net.minecraft.client.renderer.*;
 import net.minecraft.client.renderer.entity.ItemRenderer;
 import net.minecraft.client.resources.model.BakedModel;
@@ -16,54 +11,48 @@ import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
+import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(ItemRenderer.class)
 public abstract class ItemRendererMixin {
     @Shadow @Final private ItemModelShaper itemModelShaper;
 
-    @Inject(
+    @ModifyVariable(
         method = "render",
-        at = @At("HEAD")
+        at = @At("HEAD"),
+        argsOnly = true,
+        ordinal = 0
     )
-    private void onRender(
-        ItemStack stack,
-        ItemDisplayContext displayContext,
-        boolean leftHand,
-        PoseStack poseStack,
-        MultiBufferSource buffer,
-        int combinedLight,
-        int combinedOverlay,
+    private BakedModel onRender(
         BakedModel model,
-        CallbackInfo ci,
-        @Local(argsOnly = true) LocalRef<BakedModel> modelRef
+        ItemStack stack,
+        ItemDisplayContext displayContext
     ) {
-        boolean simple = displayContext == ItemDisplayContext.GUI || displayContext == ItemDisplayContext.GROUND || displayContext == ItemDisplayContext.FIXED;
-        if (!simple) return;
+        boolean simple = displayContext == ItemDisplayContext.GUI ||
+            displayContext == ItemDisplayContext.GROUND ||
+            displayContext == ItemDisplayContext.FIXED;
 
-        if (GameRendering.SPECIAL_ITEMS.containsKey(stack.getItem())) {
-            modelRef.set(this.itemModelShaper.getItemModel(stack));
+        if (simple &&GameRendering.HAND_HELD_MODELS.containsKey(stack.getItem())) {
+            return this.itemModelShaper.getItemModel(stack);
         }
+
+        return model;
     }
 
-    @WrapOperation(
+    @Redirect(
         method = "getModel",
         at = @At(
             value = "INVOKE",
             target = "Lnet/minecraft/client/renderer/ItemModelShaper;getItemModel(Lnet/minecraft/world/item/ItemStack;)Lnet/minecraft/client/resources/model/BakedModel;"
         )
     )
-    private BakedModel onGettingModel(
-        ItemModelShaper shaper,
-        ItemStack stack,
-        Operation<BakedModel> original
-    ) {
+    private BakedModel onGettingModel(ItemModelShaper shaper, ItemStack stack) {
         Item item = stack.getItem();
-        if (GameRendering.SPECIAL_ITEMS.containsKey(item)) {
-            return shaper.getModelManager().getModel(GameRendering.SPECIAL_ITEMS.get(item));
+        if (GameRendering.HAND_HELD_MODELS.containsKey(item)) {
+            return shaper.getModelManager().getModel(GameRendering.HAND_HELD_MODELS.get(item));
         } else {
-            return original.call(shaper, stack);
+            return shaper.getItemModel(stack);
         }
     }
 }
