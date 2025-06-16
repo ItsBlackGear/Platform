d1eedce67af11e5ce06a21af93453e3ddd9b1976
ItsBlackGear <68313143+ItsBlackGear@users.noreply.github.com>
2025-06-16 18:40:30 -0400

    📚 Fixed creative tab ordering and forge not working

--- fabric/src/main/java/com/blackgear/platform/common/fabric/CreativeTabsImpl.java
+++ fabric/src/main/java/com/blackgear/platform/common/fabric/CreativeTabsImpl.java
@@ -3,7 +3,6 @@ package com.blackgear.platform.common.fabric;
 import com.blackgear.platform.common.CreativeTabs;
 import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
-import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.ItemStack;
@@ -11,7 +10,6 @@ import net.minecraft.world.item.ItemStack;
 import java.util.List;
 import java.util.function.Consumer;
 
-@SuppressWarnings("UnstableApiUsage")
 public class CreativeTabsImpl {
     public static CreativeModeTab create(Consumer<CreativeModeTab.Builder> consumer) {
         CreativeModeTab.Builder builder = FabricItemGroup.builder();
@@ -19,31 +17,31 @@ public class CreativeTabsImpl {
         return builder.build();
     }
     
-    public static void modify(CreativeModeTab tab, CreativeTabs.Modifier modifier) {
-        BuiltInRegistries.CREATIVE_MODE_TAB.getResourceKey(tab).ifPresent(key -> modify(key, modifier));
-    }
-    
     public static void modify(ResourceKey<CreativeModeTab> key, CreativeTabs.Modifier modifier) {
         ItemGroupEvents.modifyEntriesEvent(key).register(entries -> {
-            modifier.accept(entries.getEnabledFeatures(), new CreativeTabs.Output() {
-                @Override
-                public void addAfter(ItemStack target, ItemStack stack, CreativeModeTab.TabVisibility visibility) {
-                    if (target.isEmpty()) {
-                        entries.accept(stack, visibility);
-                    } else {
-                        entries.addAfter(target, List.of(stack), visibility);
+            modifier.accept(
+                entries.getEnabledFeatures(),
+                new CreativeTabs.Output() {
+                    @Override
+                    public void addAfter(ItemStack target, ItemStack stack, CreativeModeTab.TabVisibility visibility) {
+                        if (target.isEmpty()) {
+                            entries.accept(stack, visibility);
+                        } else {
+                            entries.addAfter(target, List.of(stack), visibility);
+                        }
                     }
-                }
-                
-                @Override
-                public void addBefore(ItemStack target, ItemStack stack, CreativeModeTab.TabVisibility visibility) {
-                    if (target.isEmpty()) {
-                        entries.accept(stack, visibility);
-                    } else {
-                        entries.addBefore(target, List.of(stack), visibility);
+
+                    @Override
+                    public void addBefore(ItemStack target, ItemStack stack, CreativeModeTab.TabVisibility visibility) {
+                        if (target.isEmpty()) {
+                            entries.accept(stack, visibility);
+                        } else {
+                            entries.addBefore(target, List.of(stack), visibility);
+                        }
                     }
-                }
-            }, entries.shouldShowOpRestrictedItems());
+                },
+                entries.shouldShowOpRestrictedItems()
+            );
         });
     }
 }
--- forge/src/main/java/com/blackgear/platform/common/forge/CreativeTabsImpl.java
+++ forge/src/main/java/com/blackgear/platform/common/forge/CreativeTabsImpl.java
@@ -1,16 +1,19 @@
 package com.blackgear.platform.common.forge;
 
+import com.blackgear.platform.Platform;
 import com.blackgear.platform.common.CreativeTabs;
-import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
 
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.function.Consumer;
 
+@Mod.EventBusSubscriber(modid = Platform.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class CreativeTabsImpl {
     private static final Set<Consumer<BuildCreativeModeTabContentsEvent>> MODIFICATIONS = ConcurrentHashMap.newKeySet();
     
@@ -19,34 +22,39 @@ public class CreativeTabsImpl {
         consumer.accept(builder);
         return builder.build();
     }
-    
+
     public static void modify(ResourceKey<CreativeModeTab> key, CreativeTabs.Modifier modifier) {
-        modify(BuiltInRegistries.CREATIVE_MODE_TAB.get(key), modifier);
-    }
-    
-    public static void modify(CreativeModeTab tab, CreativeTabs.Modifier modifier) {
         MODIFICATIONS.add(event -> {
-            if (event.getTab() == tab) {
-                modifier.accept(event.getFlags(), new CreativeTabs.Output() {
-                    @Override
-                    public void addAfter(ItemStack target, ItemStack stack, CreativeModeTab.TabVisibility visibility) {
-                        if (target.isEmpty()) {
-                            event.getEntries().put(stack, visibility);
-                        } else {
-                            event.getEntries().putAfter(target, stack, visibility);
+            if (event.getTabKey().equals(key)) {
+                modifier.accept(
+                    event.getFlags(),
+                    new CreativeTabs.Output() {
+                        @Override
+                        public void addAfter(ItemStack target, ItemStack stack, CreativeModeTab.TabVisibility visibility) {
+                            if (target.isEmpty()) {
+                                event.getEntries().put(stack, visibility);
+                            } else {
+                                event.getEntries().putAfter(target, stack, visibility);
+                            }
                         }
-                    }
-                    
-                    @Override
-                    public void addBefore(ItemStack target, ItemStack stack, CreativeModeTab.TabVisibility visibility) {
-                        if (target.isEmpty()) {
-                            event.getEntries().put(stack, visibility);
-                        } else {
-                            event.getEntries().putBefore(target, stack, visibility);
+
+                        @Override
+                        public void addBefore(ItemStack target, ItemStack stack, CreativeModeTab.TabVisibility visibility) {
+                            if (target.isEmpty()) {
+                                event.getEntries().put(stack, visibility);
+                            } else {
+                                event.getEntries().putBefore(target, stack, visibility);
+                            }
                         }
-                    }
-                }, event.hasPermissions());
+                    },
+                    event.hasPermissions()
+                );
             }
         });
+    }
+
+    @SubscribeEvent
+    public static void onLootTableModify(BuildCreativeModeTabContentsEvent event) {
+        MODIFICATIONS.forEach(consumer -> consumer.accept(event));
     }
 }
